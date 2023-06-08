1434effa304134e3a7a8d548adb3ee605649105d
vutuhuy26 <vutuhuy260@gmail.com>
2023-06-08 09:24:40 +0700

    update

--- QL_MuaSam/DBEngine.java
+++ QL_MuaSam/DBEngine.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package QL_DanhSachTaiSan;
+package QL_MuaSam;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
